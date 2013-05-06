eda6f91120185277b487bd2a8c1b1d59e9dd1061
Joe Andrews <joe@fuuzik.com>
2013-05-06 14:44:38 +0200

    working upload, need to implement delete, cleanup, pause resume and progress indicators

--- s3upload.js
+++ s3upload.js
@@ -11,8 +11,15 @@ var initialize = function(evt){
 		//save chunk ids with chunk numbers as private table.
 		that.response = function(){
 			console.log(this.responseXML);
-			uploads3.id = this.responseXML.getElementsByTagName("UploadId")[0].childNodes[0].nodeValue;
+			uploads3.id = encodeURI(this.responseXML.getElementsByTagName("UploadId")[0].childNodes[0].nodeValue);
+			uploads3.path = encodeURI(this.responseXML.getElementsByTagName("Key")[0].childNodes[0].nodeValue);
 			that.startupload();
+		};
+		that.getid = function(){
+			return uploads3.id;
+		};
+		that.getpath = function(){
+			return uploads3.path;
 		};
 		
 		that.setup =function(){
@@ -22,9 +29,10 @@ var initialize = function(evt){
 			//custom certification goes here, currently developing a node module to handle this.
 			that.uploadoptions = {
             	type:"POST",
-            	path:"/mixes/"+urlEncode(that.file.name)+"?uploads",
+            	path:encodeURI("/mixes/"+that.file.name),
             	datatype:"binary/octel-stream",
-            	bucket:"fuuzik"
+            	bucket:"fuuzik",
+            	endings:"?uploads"
             };
 			$.ajax({
             url: "/certif",
@@ -34,11 +42,9 @@ var initialize = function(evt){
             data:JSON.stringify(that.uploadoptions),
             success: function(data){
             	console.log(data);
-            that.data = data;	
-
 	       	var request = new XMLHttpRequest();
 	       	request.withCredentials = true;
-	       	request.open("POST", "http://"+data.s3Policy.conditions[0].bucket+".s3.amazonaws.com"+that.uploadoptions.path+"", true);	      	
+	       	request.open("POST", "http://"+that.uploadoptions.bucket+".s3.amazonaws.com"+that.uploadoptions.path+"?uploads",true);	      	
 	       	request.setRequestHeader("Authorization", "AWS "+data.s3Key+":"+data.s3Signature+"");
 	  		request.setRequestHeader("X-Amz-Date" , data.s3Policy.expires);
 	      	request.setRequestHeader("Content-Type","binary/octel-stream");
@@ -52,63 +58,234 @@ var initialize = function(evt){
             }
         });
 		};
+		that.getparts = function(id,path,type){
+			
+			that.uploadoptions = {
+            	type:type,
+            	path:"/"+path.replace(/\s/g, ''),
+            	bucket:"fuuzik",
+            	endings:"?uploadId="+id.replace(/\s/g, '')
+            };
+			$.ajax({
+            url: "/certif",
+            dataType: "JSON",
+            type:"POST",
+            contentType: "application/json",
+            data:JSON.stringify(that.uploadoptions),
+            success: function(data){
+            	
+			var send = new XMLHttpRequest();
+			send.withCredentials = true;
+			send.open(type,"http://"+that.uploadoptions.bucket+".s3.amazonaws.com"+[that.uploadoptions.path]+""+[that.uploadoptions.endings]+"",true);
+			send.setRequestHeader("Authorization", "AWS "+data.s3Key+":"+data.s3Signature+"");
+	  		send.setRequestHeader("X-Amz-Date" , data.s3Policy.expires);
+	      	
+	      	
+			send.onload = function(){
+				console.log(this);
+				that.parts = this.responseXML.getElementsByTagName("Part");
+				console.log(this.responseXML);
+				
+			};
+			send.send();
+	       	
+
+            },
+            error: function (res, status, error) {
+                console.log(res);
+                console.log("ERROR: " + error + " status: " + status + " response: " + res);
+            }
+        });
+	
+		};
+		that.getbuckets = function(){
+			var options = {
+            	type:"GET",
+            	path:"/",
+            	bucket:"fuuzik",
+            	endings:"?uploads"
+            };
+
+			$.ajax({
+            url: "/certif",
+            dataType: "JSON",
+            type:"POST",
+            contentType: "application/json",
+            data:JSON.stringify(options),
+            success: function(data){
+            	
+
+			var send = new XMLHttpRequest();
+			send.withCredentials = true;
+			send.open("GET","http://"+options.bucket+".s3.amazonaws.com"+options.path+""+options.endings+"",true);
+			send.setRequestHeader("Authorization", "AWS "+data.s3Key+":"+data.s3Signature+"");
+	  		send.setRequestHeader("X-Amz-Date" , data.s3Policy.expires);
+	      	
+			send.onload = function(){
+				console.log(this.responseXML);
+				
+				var uploads = this.responseXML.getElementsByTagName("Upload");
+				for (var i = 0; i < uploads.length; i++) {
+				
+					var options = {
+		            	type:"DELETE",
+		            	path:encodeURI("/"+uploads[i].getElementsByTagName("Key")[0].childNodes[0].nodeValue),
+		            	
+		            	bucket:"fuuzik",
+		            	endings:encodeURI("?uploadId="+uploads[i].getElementsByTagName("UploadId")[0].childNodes[0].nodeValue)
+		            };
+		            that.getparts(uploads[i].getElementsByTagName("UploadId")[0].childNodes[0].nodeValue.toString(),uploads[i].getElementsByTagName("Key")[0].childNodes[0].nodeValue.toString(),"DELETE");
+				};
+			
+				
+			};
+			send.send();
+	       	
+
+            },
+            error: function (res, status, error) {
+                console.log(res);
+                console.log("ERROR: " + error + " status: " + status + " response: " + res);
+            }
+        });
+
+		};
 		//send each chunk
 		that.eachchunk = function(blob){
-			
+			var options = {
+            	type:"PUT",
+            	path:"/"+that.getpath(),
+            
+            	bucket:"fuuzik",
+            	endings:encodeURI("?partNumber="+blob.index+"&uploadId="+that.getid().replace(/\s/g, ''))
+            };
+
+			$.ajax({
+            url: "/certif",
+            dataType: "JSON",
+            type:"POST",
+            contentType: "application/json",
+            data:JSON.stringify(options),
+            success: function(data){
+            	
+            var progressFunction = function(e){
+            	console.log((e.loaded/that.SIZE)*100);
+
+            };
 			var send = new XMLHttpRequest();
-			send.open("POST","http://"+that.data.s3Policy.conditions[0].bucket+".s3.amazonaws.com"+that.uploadoptions.path+"?partNumber="+blob.index+"&uploadId="+blob.parentid+" HTTP/1.1", true);
-			send.setRequestHeader("Authorization", "AWS "+that.data.s3Key+":"+that.data.s3Signature+"");
-	  		send.setRequestHeader("X-Amz-Date" , that.data.s3Policy.expires);
-	      	send.setRequestHeader("Content-Type","binary/octel-stream");
+			send.withCredentials = true;
+			send.open("PUT","http://"+options.bucket+".s3.amazonaws.com"+options.path+options.endings,true);
+			send.setRequestHeader("Authorization", "AWS "+data.s3Key+":"+data.s3Signature);
+	  		send.setRequestHeader("X-Amz-Date" , data.s3Policy.expires);
+	      	send.onreadystatechange=function(e){
+	      			console.log(e);
+	      			console.log(this.getAllResponseHeaders());
+	      			that.chunks[blob.index] = blob;
+	      			that.chunks[blob.index].etag = this.getAllResponseHeaders();
+	      	};
+	      	 
+
+    		send.upload.addEventListener("progress", progressFunction, false);  
+    		
+    		
+
 			send.onload = function(){
-				that.chunks[blob.index] = blob;
-				that.chunks[blob.index].etag = this.responseXML.getElementsByTagName("ETag")[0].childNodes[0].nodeValue;
+				//console.log(this.responseXML.getElementsByTagName("ETag")[0].childNodes[0].nodeValue);
+				
+				
 			};
-			send.send(blob.blob);	
+			send.send(blob.blob);
+
+	       	
+
+            },
+            error: function (res, status, error) {
+                console.log(res);
+                console.log("ERROR: " + error + " status: " + status + " response: " + res);
+            }
+        });
+	
 		};
 		that.chunks = [];
 
-		that.complete = function(){
-			var xml = "<CompleteMultipartUpload>";
-			for (var i = 0; i < that.chunks.length; i++) {
-				xml = xml+"<Part><PartNumber>"+i+"</PartNumber><ETag>"+that.chunks[i].etag+"</ETag></Part>";
-				
+		that.complete = function(parts,path,id){
+
+
+			that.uploadoptions = {
+            	type:"POST",
+            	path:"/"+path,
+            	datatype:"application/xml",
+            	bucket:"fuuzik",
+            	endings:"?uploadId="+id
+            };
+			$.ajax({
+            url: "/certif",
+            dataType: "JSON",
+            type:"POST",
+            contentType: "application/json",
+            data:JSON.stringify(that.uploadoptions),
+            success: function(data){
+            	
+            	var xml = "<CompleteMultipartUpload>";
+			for (var i = 0; i < parts.length; i++) {
+				xml = xml+"<Part>";
+				xml = xml+"<PartNumber>"+parts[i].getElementsByTagName("PartNumber")[0].childNodes[0].nodeValue+"</PartNumber>";
+				xml = xml+"<ETag>"+parts[i].getElementsByTagName("ETag")[0].childNodes[0].nodeValue+"</ETag>";
+	
+				xml = xml +"</Part>";
 			};
 			xml = xml+ "</CompleteMultipartUpload>";
+
+			console.log(StringtoXML(xml));
 			var complete = new XMLHttpRequest();
-				complete.open("POST", "http://"+data.s3Policy.conditions[0].bucket+".s3.amazonaws.com"+that.uploadoptions.path+"?uploadId="+uploads3.id+"", true);	     
-				complete.setRequestHeader("Authorization", "AWS "+that.data.s3Key+":"+that.data.s3Signature+"");
-		  		complete.setRequestHeader("X-Amz-Date" , that.data.s3Policy.expires);
-		      	complete.setRequestHeader("Content-Type","binary/octel-stream");
+				complete.open("POST", "http://"+that.uploadoptions.bucket+".s3.amazonaws.com"+[that.uploadoptions.path]+""+[that.uploadoptions.endings]+"",true);	     
+				complete.setRequestHeader("Authorization", "AWS "+data.s3Key+":"+data.s3Signature+"");
+		  		complete.setRequestHeader("X-Amz-Date" , data.s3Policy.expires);
+		      	complete.setRequestHeader("Content-Type","application/xml");
 				complete.onload = function(){
 					console.log(this.responseXML);
 				};
-				complete.send();	
+				complete.send(StringtoXML(xml));	
+	       	
+
+            },
+            error: function (res, status, error) {
+                console.log(res);
+                console.log("ERROR: " + error + " status: " + status + " response: " + res);
+            }
+        });
+			
 		};
 		// called when a file is selected so this points to the event
 		that.startupload = function(){
 
-			that.BYTES_PER_CHUNK = 1024 * 1024; // 1MB chunk sizes.
+			that.BYTES_PER_CHUNK = 1024 * 1024 * 6; // 1MB chunk sizes.
 	  		that.SIZE = that.file.size;
 	  		that.nochunks = Math.round(that.file.size/that.BYTES_PER_CHUNK);
 	  		that.start = 0;
 	  		that.end = that.BYTES_PER_CHUNK;
 
-	  		for (var i = 0; i < that.nochunks; i++) {
-	  		that.chunks.push({
+	  		for (var i = 1; i < that.nochunks+1; i++) {
+	  		if (i<that.nochunks+1) {
+	  			that.eachchunk({
 	    			blob:that.file.slice((i*that.BYTES_PER_CHUNK), (i+1)*that.BYTES_PER_CHUNK),
 	    			index:i,
-	    			parentid:uploads3.id
-
-	    		});
-			that.eachchunk({
-	    			blob:that.file.slice((i*that.BYTES_PER_CHUNK), (i+1)*that.BYTES_PER_CHUNK),
-	    			index:i,
-	    			parentid:uploads3.id
+	    			parentid:uploads3.id,
+	    			size:that.BYTES_PER_CHUNK
 
 	    		});
 
-	  				};
+	  		};
+			
+			if (i ===that.nochunks) {
+				that.eachchunk({
+	    			blob:that.file.slice((i*that.BYTES_PER_CHUNK), that.SIZE),
+	    			index:i,
+	    			parentid:uploads3.id,
+	    			size:that.SIZE-(i*that.BYTES_PER_CHUNK)
+	    		});
+			};
+	  		};
 	  			
 		};
 
@@ -156,4 +333,14 @@ cancel:function(){
 return initialize;
 })();
 
-
+function StringtoXML(text){
+                if (window.ActiveXObject){
+                  var doc=new ActiveXObject('Microsoft.XMLDOM');
+                  doc.async='false';
+                  doc.loadXML(text);
+                } else {
+                  var parser=new DOMParser();
+                  var doc=parser.parseFromString(text,'text/xml');
+                }
+                return doc;
+            }
